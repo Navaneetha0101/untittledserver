81e968f3351ac95531dbd36535a73f4c6205166d
Navaneetha <94621898+Navaneetha0101@users.noreply.github.com>
2024-05-12 13:48:55 +0530

    todeploy

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const corsOptions = {
     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials:true,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", authRouter);
