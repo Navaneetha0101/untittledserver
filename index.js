d216ecdfe119bcbf0a19d2cf6503559132047abe
Navaneetha <94621898+Navaneetha0101@users.noreply.github.com>
2024-05-12 13:32:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const connectDb = require("./utility/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5174",http://localhost:5173"],
     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials:true,
 };
