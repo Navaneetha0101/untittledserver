f096818f0774ebe81a8b2b0a8e47076f7bfeb98a
navaneetha <navaneetha@yrpitsolutions>
2024-05-17 16:38:52 +0530

    to deploy

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const contactRoute = require("./router/contact-router");
 const connectDb = require("./utility/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
-const corsOptions = {
-    origin:["https://untittled-one.vercel.app/","http://localhost:5174","http://localhost:5173"],
-    methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials:true,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin:["https://untittled-one.vercel.app/","http://localhost:5174","http://localhost:5173"],
+//     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
+//     credentials:true,
+// };
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", authRouter);
 app.use("/form", contactRoute);
